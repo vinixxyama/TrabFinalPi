9a6ce7815bc8b80e356632608364078fbbc947a5
Francisco Pegorel <pegorel@gmail.com>
2019-01-27 04:46:52 -0200

    Segmentação dos pontos na imagem segunda.jpg
    Aplicação do graytresh e alteração do raio segmentado

--- projetopi.m
+++ projetopi.m
@@ -5,9 +5,8 @@ clc
 
 pkg image load
 
-domino=imread('45.jpg');
-bdomino = im2bw(domino);
-dominogray = rgb2gray(domino);
+domino=imread('segunda.jpg');
+domino = im2bw(domino, graythresh(domino));
 %[rows, columns, numberOfColorChannels] = size(dominogray);
 %topHalf = imcrop(dominogray, [1, 1, columns, floor(rows/2)]);
 %bottomHalf = imcrop(dominogray, [1, ceil(rows/2), columns, floor(rows/2)]);
@@ -17,14 +16,15 @@ n=fix(size(domino,1)/2);
 A=domino(1:n,:,:);
 B=domino(n+1:end,:,:);
 imshow(A);
-[centers,radii] = imfindcircles(A,[4 8],'ObjectPolarity','dark','sensitivity',0.9);
+
+[centers,radii] = imfindcircles(A,[20 50],'ObjectPolarity','dark','sensitivity',0.5);
 h = viscircles(centers, radii);
 tam = length(centers);
 caption = sprintf('valor = %d', tam);
 text(50, 10, caption, 'FontSize', 20);
 figure;
 imshow(B);
-[centers,radii] = imfindcircles(B,[4 8],'ObjectPolarity','dark','sensitivity',0.9);
+[centers,radii] = imfindcircles(B,[20 50],'ObjectPolarity','dark','sensitivity',0.5);
 h = viscircles(centers, radii);
 tam = length(centers);
 caption = sprintf('valor = %d', tam);
