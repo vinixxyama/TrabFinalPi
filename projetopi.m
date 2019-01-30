93cb737f9e5fefbb7fdff3390184e0face6fb1ed
vinixxyama <vini_yamamoto@hotmail.com>
2019-01-29 23:53:54 -0200

    fotos com dominos

--- projetopi.m
+++ projetopi.m
@@ -5,30 +5,34 @@ clc
 
 pkg image load
 
-domino=imread('segunda.jpg');
-domino = im2bw(domino, graythresh(domino));
-%[rows, columns, numberOfColorChannels] = size(dominogray);
-%topHalf = imcrop(dominogray, [1, 1, columns, floor(rows/2)]);
-%bottomHalf = imcrop(dominogray, [1, ceil(rows/2), columns, floor(rows/2)]);
-%imshow(topHalf);
-%imshow(bottomHalf);
-n=fix(size(domino,1)/2);
-A=domino(1:n,:,:);
-B=domino(n+1:end,:,:);
-imshow(A);
+domino=imread('3.jpg');
+domino = im2bw(domino);
 
-[centers,radii] = imfindcircles(A,[20 50],'ObjectPolarity','dark','sensitivity',0.5);
+[centers,radii] = imfindcircles(domino,[38 55],'ObjectPolarity','dark','sensitivity',0.9);
+imshow(domino);
 h = viscircles(centers, radii);
 tam = length(centers);
 caption = sprintf('valor = %d', tam);
-text(50, 10, caption, 'FontSize', 20);
-figure;
-imshow(B);
-[centers,radii] = imfindcircles(B,[20 50],'ObjectPolarity','dark','sensitivity',0.5);
-h = viscircles(centers, radii);
-tam = length(centers);
-caption = sprintf('valor = %d', tam);
-text(50, 0, caption, 'FontSize', 20);
-fullImage = cat(2,B,A);
-figure;
-imshow(fullImage);
+text(0, 10, caption, 'FontSize', 20);
+%n=fix(size(domino,1)/2);
+%A=domino(1:n,:,:);
+%B=domino(n+1:end,:,:);
+%imshow(A);
+%[centers,radii] = imfindcircles(A,[5 10],'ObjectPolarity','dark','sensitivity',0.9);
+%h = viscircles(centers, radii);
+%tam = length(centers);
+%caption = sprintf('valor = %d', tam);
+%text(50, 10, caption, 'FontSize', 20);
+%figure;
+%imshow(B);
+%[centers,radii] = imfindcircles(B,[20 50],'ObjectPolarity','dark','sensitivity',0.5);
+%h = viscircles(centers, radii);
+%tam = length(centers);
+%caption = sprintf('valor = %d', tam);
+%text(50, 0, caption, 'FontSize', 20);
+%fullImage = cat(2,B,A);
+%figure;
+%imshow(fullImage);
+
+%segchannel segmentaçcao por canal de cor
+
