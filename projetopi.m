2b5185e844c5f71d87d4dba7f27f5d1b485c8329
Francisco Pegorel <pegorel@gmail.com>
2019-01-31 00:54:17 -0200

    Selecionando a bolinha da imagem 1.jpg

--- projetopi.m
+++ projetopi.m
@@ -9,19 +9,37 @@ pkg image load
 %subtrair a primeira com a proxima jogada
 %salvar a primeira jogada em uma variavel
 %sempre subtrair a proxima jogada da anterior e manter a primeira jogada para saber uma das extremidades
-domino=imread('2.jpg');
-domino2=imread('3.jpg');
-domino2=rgb2gray(domino2);
-domino=rgb2gray(domino);
-domino3 = domino2-domino;
-imshow(domino3);
-figure;
-[centers,radii] = imfindcircles(domino3,[30 50],'ObjectPolarity','dark','sensitivity',0.9);
-imshow(domino3);
+
+frame = javaObject("javax.swing.JFrame");
+frame.setBounds(0,0,100,100);
+frame.setVisible(true);
+fc = javaObject ("javax.swing.JFileChooser")
+returnVal = fc.showOpenDialog(frame);
+file = fc.getSelectedFile();
+file.getName()
+
+domino = imread('1.jpg');
+domino = im2bw(domino, graythresh(domino));
+imshow(domino);
+[centers,radii] = imfindcircles(domino,[60 120],'ObjectPolarity','dark','sensitivity',0.85);
 h = viscircles(centers, radii);
 tam = length(centers);
-caption = sprintf('valor = %d', tam);
-text(0, 10, caption, 'FontSize', 20);
+caption = sprintf('valor = %d', tam-1);
+text(50, 0, caption, 'FontSize', 20);
+
+
+%domino2=imread('3.jpg');
+%domino2=rgb2gray(domino2);
+%domino=rgb2gray(domino);
+%domino3 = domino2-domino;
+%imshow(domino3);
+%figure;
+%[centers,radii] = imfindcircles(domino3,[30 50],'ObjectPolarity','dark','sensitivity',0.9);
+%imshow(domino3);
+%h = viscircles(centers, radii);
+%tam = length(centers);
+%caption = sprintf('valor = %d', tam);
+%text(0, 10, caption, 'FontSize', 20);
 %n=fix(size(domino,1)/2);
 %A=domino(1:n,:,:);
 %B=domino(n+1:end,:,:);
