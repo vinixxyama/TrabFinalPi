e13b7683be324ce667787010b7a75335e03df186
vinixxyama <vini_yamamoto@hotmail.com>
2019-01-31 19:11:05 -0200

    limpeza do codigo

--- projetopi.m
+++ projetopi.m
@@ -27,38 +27,5 @@ tam = length(centers);
 caption = sprintf('valor = %d', tam-1);
 text(50, 0, caption, 'FontSize', 20);
 
-
-%domino2=imread('3.jpg');
-%domino2=rgb2gray(domino2);
-%domino=rgb2gray(domino);
-%domino3 = domino2-domino;
-%imshow(domino3);
-%figure;
-%[centers,radii] = imfindcircles(domino3,[30 50],'ObjectPolarity','dark','sensitivity',0.9);
-%imshow(domino3);
-%h = viscircles(centers, radii);
-%tam = length(centers);
-%caption = sprintf('valor = %d', tam);
-%text(0, 10, caption, 'FontSize', 20);
-%n=fix(size(domino,1)/2);
-%A=domino(1:n,:,:);
-%B=domino(n+1:end,:,:);
-%imshow(A);
-%[centers,radii] = imfindcircles(A,[5 10],'ObjectPolarity','dark','sensitivity',0.9);
-%h = viscircles(centers, radii);
-%tam = length(centers);
-%caption = sprintf('valor = %d', tam);
-%text(50, 10, caption, 'FontSize', 20);
-%figure;
-%imshow(B);
-%[centers,radii] = imfindcircles(B,[20 50],'ObjectPolarity','dark','sensitivity',0.5);
-%h = viscircles(centers, radii);
-%tam = length(centers);
-%caption = sprintf('valor = %d', tam);
-%text(50, 0, caption, 'FontSize', 20);
-%fullImage = cat(2,B,A);
-%figure;
-%imshow(fullImage);
-
 %segchannel segmentaçcao por canal de cor
 
